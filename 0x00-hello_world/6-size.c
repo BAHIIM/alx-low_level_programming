a0a2bd9a2a51d1233c685f28791c5a56f56a42f7
BAHIIM <abderahimraja2001@gmail.com>
2023-10-05 13:38:09 -0700

    f

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	Printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	Printf("Size of a int: %lu byte(s)\n", sizeof(int));
+	Printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	Printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	Printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
