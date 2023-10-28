9f4a8f47ca93a804f3ff834eb7278398de34af2f
BAHIIM <abderahimraja2001@gmail.com>
2023-10-27 17:53:11 -0700

    fd

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -3,10 +3,11 @@
 
 /**
  * _puts - Prints a string to stdout
- * @str: The string to be printed
+ * @str: The string to be printed (unused parameter)
  */
 void _puts(char *str)
 {
+	(void)str;/* This line indicates that the parameter is intentionally unused */
 	/* Implementation of the _puts function */
 }
 
