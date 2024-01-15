1b19732e005d505f99f3a6c5ed6f55789c05a2d0
BAHIIM <abde2001raja@gmail.com>
2024-01-15 14:06:49 -0800

    cdd

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string to stdout
+ * @str: The string to be printed (unused parameter)
+ */
+void _puts(char *str)
+{
+	(void)str;/* This line indicates that the parameter is intentionally unused */
+	/* Implementation of the _puts function */
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
